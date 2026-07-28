51a73b2b68f4681b61d581523f9218d8e8447b7c
u13808 <2987871874@qq.com>
2026-07-29 05:16:42 +0800

    🐱 修复: 使用原始内容重新上传 build.gradle.kts（之前被双重base64编码）

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,4 @@
-cGx1Z2lucyB7CiAgICBpZCgiY29tLmFuZHJvaWQuYXBwbGljYXRpb24iKSB2ZXJzaW9uICI4LjIuMCIgYXBwbHkgZmFsc2UKICAgIGlkKCJvcmcuamV0YnJhaW5zLmtvdGxpbi5hbmRyb2lkIikgdmVyc2lvbiAiMS45LjIyIiBhcHBseSBmYWxzZQp9
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
